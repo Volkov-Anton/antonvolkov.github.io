61c16ce7dc61f57af2e371adc589ee055d6ce000
Anton Volkov <151880490+Volkov-Anton@users.noreply.github.com>
2024-01-12 07:53:27 +0100

    first load

---
+++ styles.js
@@ -0,0 +1,29 @@
+SCRTIPT.JS
+document.addEventListener('DOMContentLoaded', () => {
+    let loadingBar = document.getElementById('loading-bar');
+    let loadingPercent = document.getElementById('loading-percent');
+    let percent = 0;
+
+    let loadingInterval = setInterval(() => {
+        percent++;
+        loadingBar.style.width = percent + '%';
+        loadingPercent.innerText = percent + '%';
+
+        if (percent === 100) {
+            clearInterval(loadingInterval);
+            document.getElementById('loading').style.display = 'none';
+        }
+    }, 40);
+
+    setTimeout(() => {
+        clearInterval(loadingInterval);
+        percent = 99;
+        loadingBar.style.width = percent + '%';
+        loadingPercent.innerText = percent + '%';
+    }, 3960);
+});
+
+
+
+
+
